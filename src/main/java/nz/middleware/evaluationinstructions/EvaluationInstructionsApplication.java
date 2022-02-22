388e48afad66b31c75d7efa059c4365dc5d9d0b8
BuildTools <unconfigured@null.spigotmc.org>
2022-02-22 15:26:34 +1300

    openapi generation

---
+++ src/main/java/nz/middleware/evaluationinstructions/EvaluationInstructionsApplication.java
@@ -0,0 +1,13 @@
+package nz.middleware.evaluationinstructions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvaluationInstructionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvaluationInstructionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nz/middleware/evaluationinstructions/EvaluationInstructionsApplicationTests.java
@@ -0,0 +1,13 @@
+package nz.middleware.evaluationinstructions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvaluationInstructionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
